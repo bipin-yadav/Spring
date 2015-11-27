0cb4947332ec784895ad92eacaad4eb310c200fe
Bipin Yadav <bipin-yadav@users.noreply.github.com>
2015-11-27 10:26:12 +0530

    Update Person.java

--- Sample Projects/1.0-Spring-Tutorial/src/main/java/com/javasnippet/spring/test/Person.java
+++ Sample Projects/1.0-Spring-Tutorial/src/main/java/com/javasnippet/spring/test/Person.java
@@ -3,7 +3,7 @@ package com.javasnippet.spring.test;
 public class Person {
 	
 	public void speak(){
-		System.out.println("Hello i am person 1");
+		System.out.println("Hey... This is Bipin");
 	}
 
 }
